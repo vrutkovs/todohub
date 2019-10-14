6adb9041feda6c5b96403e1a699aa3af97f652ef
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-15 01:21:06 +0200

    github: omit empty search prefix

--- pkg/github/settings.go
+++ pkg/github/settings.go
@@ -2,6 +2,6 @@ package github
 
 type GithubSettings struct {
 	Token            string            `yaml:"token"`
-	SearchPrefix     string            `yaml:"search_prefix"`
+	SearchPrefix     string            `yaml:"search_prefix,omitempty"`
 	GithubSearchList map[string]string `yaml:"lists"`
 }
